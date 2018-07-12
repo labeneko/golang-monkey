c4788be7ce0d32b16c06d8f346c8ae4b0cc0c124
Hidenori Miyoshi <miyohide@gmail.com>
2018-07-12 21:00:16 +0900

    P10-11. implement default case

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -41,9 +41,21 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
+}
+
+// 英文字 + '_' であればtrue、それ以外はfalse
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
@@ -60,4 +72,13 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition++
+}
+
+// 英字 + '_' 以外の文字が出てくるまで読み込み、識別子/キーワードとして返す
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
 }
