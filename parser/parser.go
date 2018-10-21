1cb2d7a4aa9b313e99572b5af92821a57b4f1b5b
Hidenori Miyoshi <miyohide@gmail.com>
2018-10-21 23:56:49 +0900

    P111. delete todo.

--- parser/parser.go
+++ parser/parser.go
@@ -121,10 +121,15 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
-	// TODO セミコロンに遭遇するまで式を読み飛ばしてしまっている
-	for !p.curTokenIs(token.SEMICOLON) {
+
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
+
 	return stmt
 }
 
@@ -161,7 +166,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	// TODO セミコロンに遭遇するまで四季を読み飛ばしてしまっている
+	stmt.ReturnValue = p.parseExpression(LOWEST)
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
