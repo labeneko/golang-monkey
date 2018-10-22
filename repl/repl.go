59f39bfc73d3e801ae24c482693ba18aaae9913a
Hidenori Miyoshi <miyohide@gmail.com>
2018-10-22 21:16:11 +0900

    P112. implement RPPL.

--- repl/repl.go
+++ repl/repl.go
@@ -3,10 +3,10 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/miyohide/monkey/parser"
 	"io"
 
 	"github.com/miyohide/monkey/lexer"
-	"github.com/miyohide/monkey/token"
 )
 
 // PROMPT はREPLで入力を促すプロンプト
@@ -24,9 +24,23 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+// printParserErrors は第一引数で取った io.Writer に対して
+// 第二引数の []string の内容を整形して出力する
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
