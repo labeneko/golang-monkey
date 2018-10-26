6a08085c69935f5151fca886115992b56ddb7acb
Hidenori Miyoshi <miyohide@gmail.com>
2018-10-26 23:27:40 +0900

    P125. implement evaluator test.

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,47 @@
+package evaluator
+
+import (
+	"github.com/miyohide/monkey/lexer"
+	"github.com/miyohide/monkey/object"
+	"github.com/miyohide/monkey/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T)  {
+	tests := []struct {
+		input string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	// 入力を字句解析器に渡し、字句解析器を構文解析器に渡してASTを得る
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	// Evalの結果が*object.Integerであること
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+	// 値が想定値であること
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+	return true
+}
