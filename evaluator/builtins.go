babdc23605fb13697cb67568319f4268335dc919
Hidenori Miyoshi <miyohide@gmail.com>
2018-12-04 21:52:59 +0900

    P187. add buildins.go

---
+++ evaluator/builtins.go
@@ -0,0 +1,11 @@
+package evaluator
+
+import "github.com/miyohide/monkey/object"
+
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			return NULL
+		},
+	},
+}
