d61256b9f379e5740572eaadbb3409f951a4d536
Hidenori Miyoshi <miyohide@gmail.com>
2018-12-21 21:18:55 +0900

    Revert "P204. add evalArrayIndexExpression."
    This reverts commit 0a8270567d323618929fcbc0bdfc616822097196.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -332,14 +332,3 @@ func evalIndexExpression(left, index object.Object) object.Object {
 		return newError("index operator not supported: %s", left.Type())
 	}
 }
-
-func evalArrayIndexExpression(array, index object.Object) object.Object {
-	arrayObject := array.(*object.Array)
-	idx := index.(*object.Integer).Value
-	max := int64(len(arrayObject.Elements) - 1)
-
-	if idx < 0 || idx > max {
-		return NULL
-	}
-	return arrayObject.Elements[idx]
-}
