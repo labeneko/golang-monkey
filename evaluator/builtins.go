707bed7331805806aecc4f7cd042bb09928daf02
Hidenori Miyoshi <miyohide@gmail.com>
2018-12-21 21:18:40 +0900

    Revert "P205. add Array case to prepare to add buildin func."
    This reverts commit b85b6998a1e165265d6c6fcdd0cacc45f9700d64.

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -9,8 +9,6 @@ var builtins = map[string]*object.Builtin{
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
 			}
 			switch arg := args[0].(type) {
-			case *object.Array:
-				return &object.Integer{Value: int64(len(arg.Elements))}
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
