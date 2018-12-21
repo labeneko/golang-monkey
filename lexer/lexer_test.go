c00eabada7e019ea08b96227794c44ffcafc16ed
Hidenori Miyoshi <miyohide@gmail.com>
2018-12-21 21:14:51 +0900

    Revert "P212. add hash test."
    This reverts commit 3ab3474c66bf612ad75b8223072fef7661243d25.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -29,7 +29,6 @@ if (5 < 10) {
 "foobar"
 "foo bar"
 [1, 2];
-{"foo": "bar"}
 `
 
 	tests := []struct {
@@ -117,11 +116,6 @@ if (5 < 10) {
 		{token.INT, "2"},
 		{token.RBRACKET, "]"},
 		{token.SEMICOLON, ";"},
-		{token.LBRACE, "{"},
-		{token.STRING, "foo"},
-		{token.COLON, ":"},
-		{token.STRING, "bar"},
-		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
